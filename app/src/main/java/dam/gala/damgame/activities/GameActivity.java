f87c6b64cbc5077503eeffbd9811107eb0334ba9
AlexirowW <dam2aalejandrogarciabocero@iesantoniogala.es>
2021-02-17 14:30:52 +0100

    Segundo commit

--- app/src/main/java/dam/gala/damgame/activities/GameActivity.java
+++ app/src/main/java/dam/gala/damgame/activities/GameActivity.java
@@ -38,11 +38,12 @@ import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
 
 /**
  * Actividad principal
+ *
  * @author 2º DAM - IES Antonio Gala
  * @version 1.0
  */
 public class GameActivity extends AppCompatActivity implements InterfaceDialog {
-    private final int SETTINGS_ACTION =1;
+    private final int SETTINGS_ACTION = 1;
     private Play gameMove;
     private int sceneCode;
     private GameView gameView;
@@ -61,6 +62,7 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
     /**
      * Método de callback del ciclo de vida de la actividad, llamada anterior a que la actividad
      * pasé al estado 'Activa'
+     *
      * @param savedInstanceState Contenedor para paso de parámetros y guardar información entre
      *                           distintos estados de la actividad
      */
@@ -69,8 +71,8 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
         super.onCreate(savedInstanceState);
         setTema();
         setContentView(R.layout.activity_main);
-
-        Button btIniciar = findViewById(R.id.btnIniciar);
+        Button btIniciar;
+        btIniciar = findViewById(R.id.btnIniciar);
         btIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,10 +97,10 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
     /**
      * Inicio del juego
      */
-    private void startGame(){
+    private void startGame() {
         this.sceneCode = Integer.parseInt(getDefaultSharedPreferences(this).
-                getString("ambient_setting",String.valueOf(GameUtil.TEMA_VOLCANES)));
-        this.gameMove = Play.createGameMove(this,106);
+                getString("ambient_setting", String.valueOf(GameUtil.TEMA_VOLCANES)));
+        this.gameMove = Play.createGameMove(this, 106);
         this.scene = this.gameMove.getScene();
         this.config = new GameConfig(this.scene);
         this.getPlay().setConfig(this.config);
@@ -118,7 +120,7 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
     /**
      * Carga las imágenes del marcador de vidas, puntos y respuestas
      */
-    private void loadScoreComponents(){
+    private void loadScoreComponents() {
         this.lifes = new ArrayList<>();
         this.lifes.add(findViewById(R.id.ivBouncy1));
         this.lifes.add(findViewById(R.id.ivBouncy2));
@@ -130,10 +132,11 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
         this.ivPoints = findViewById(R.id.ivPoints);
         this.ivPoints.setImageBitmap(this.scene.getScorePoints());
     }
+
     /**
      * Muestra el cuadro de diálogo de la pregunta
      */
-    private void showQuestionDialog(){
+    private void showQuestionDialog() {
         //código para probar el cuadro de diálogo
         Button btPregunta = findViewById(R.id.btnIniciar);
 
@@ -152,24 +155,25 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
         //del repositorio
         CharSequence[] respuestas = new CharSequence[3];
         String enunciado = "Selecciona cuál de los siguientes planetas no está en la vía láctea";
-        respuestas[0]="Marte";
-        respuestas[1]="Nébula";
-        respuestas[2]="Mercurio";
+        respuestas[0] = "Marte";
+        respuestas[1] = "Nébula";
+        respuestas[2] = "Mercurio";
         int[] respuestasCorrectas = new int[]{1};
         Question question = new Question(enunciado, GameUtil.PREGUNTA_COMPLEJIDAD_ALTA,
-                GameUtil.PREGUNTA_SIMPLE,respuestas,respuestasCorrectas,20);
+                GameUtil.PREGUNTA_SIMPLE, respuestas, respuestasCorrectas, 20);
 
         QuestionDialogFragment qdf = new QuestionDialogFragment(question, GameActivity.this);
         qdf.setCancelable(false);
-        qdf.show(getSupportFragmentManager(),null);
+        qdf.show(getSupportFragmentManager(), null);
     }
+
     /**
      * Establece el tema seleccionado en las preferencias
      */
-    private void setTema(){
+    private void setTema() {
         this.sceneCode = Integer.parseInt(getDefaultSharedPreferences(this).
-                getString("theme_setting",String.valueOf(GameUtil.TEMA_DESIERTO)));
-        switch(this.sceneCode){
+                getString("theme_setting", String.valueOf(GameUtil.TEMA_DESIERTO)));
+        switch (this.sceneCode) {
             case GameUtil.TEMA_DESIERTO:
                 setTheme(R.style.Desert_DamGame);
                 break;
@@ -182,6 +186,7 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
         }
 
     }
+
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -189,10 +194,11 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
             hideSystemUI();
         }
     }
+
     /**
      * Elimina la barra de acción y deja el mayor área posible de pantalla libre
      */
-    public void hideSystemUI(){
+    public void hideSystemUI() {
         // Enables regular immersive mode.
         // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
         // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -208,36 +214,45 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
+
     /**
      * Obitiene la jugada actual
+     *
      * @return Devuelve la jugada actual (Play)
      */
-    public Play getPlay(){
+    public Play getPlay() {
         return this.gameMove;
     }
+
     /**
      * Obtiene la configuración del juego
+     *
      * @return Devuelve la configuración del juego (GameConfig)
      */
-    public GameConfig getGameConfig(){
+    public GameConfig getGameConfig() {
         return this.config;
     }
+
     /**
      * Obtiene el controlador audio
+     *
      * @return Devuelve el controlador de audio del juego (AudioController)
      */
-    public AudioController getAudioController(){
+    public AudioController getAudioController() {
         return this.audioController;
     }
+
     @Override
     public void setRespuesta(String respuesta) {
-        Toast.makeText(this,respuesta,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, respuesta, Toast.LENGTH_LONG).show();
         //si la respuesta es correcta hay que actualizar los puntos y las preguntas respondidas
         this.gameView.setStopGame(false);
         this.gameView.restart();
     }
+
     /**
      * Menú principal de la aplicación
+     *
      * @param menu Menú de aplicación
      * @return Devuelve true si se ha creado el menú
      */
@@ -245,18 +260,20 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
     public boolean onCreateOptionsMenu(Menu menu) {
         //TODO Esto se debe mejorar y sistituir en la interfaz por controles vistosos
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
     /**
      * Evento de selección de elemento de menú
+     *
      * @param item Item de menú
      * @return Devuelve true si se ha tratado el evento recibido
      */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.imSettings:
                 Intent preferences = new Intent(GameActivity.this, SettingsActivity.class);
                 startActivityForResult(preferences, SETTINGS_ACTION);
@@ -264,18 +281,20 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
         }
         return true;
     }
+
     /**
      * Método de callback para recibir el resultado de una intención llamada para devolver un
      * resultado
+     *
      * @param requestCode Código de la petición (int)
-     * @param resultCode Código de respuesta (int)
-     * @param data Intención que devuelve el resultado, la que produce el callback
+     * @param resultCode  Código de respuesta (int)
+     * @param data        Intención que devuelve el resultado, la que produce el callback
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode== SETTINGS_ACTION){
-            if(resultCode== Activity.RESULT_OK){
+        if (requestCode == SETTINGS_ACTION) {
+            if (resultCode == Activity.RESULT_OK) {
                 //TODO Este método hay que revisarlo y borrarlo si finalmente no se usa
             }
         }
@@ -283,10 +302,11 @@ public class GameActivity extends AppCompatActivity implements InterfaceDialog {
 
     /**
      * Actualiza las imágenes de las vidas disponibles, oculta la última imagen de las vidas
+     *
      * @param index Índice la imagen a ocultar
      */
-    public void updateLifes(Integer index){
-        if(index>=0)
+    public void updateLifes(Integer index) {
+        if (index >= 0)
             this.lifes.get(index).setVisibility(View.INVISIBLE);
     }
 }
